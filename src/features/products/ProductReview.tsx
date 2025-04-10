ba3af216ea3ff89c726e9e13fbc7f6be6212a1f8
DevCHIDI <chidimathayas@gmail.com>
2025-04-10 14:17:24 +0100

    add user profile image to review component

--- src/features/products/ProductReview.tsx
+++ src/features/products/ProductReview.tsx
@@ -1,13 +1,14 @@
 import { Star } from "lucide-react";
 import React from "react";
+import user from "../../assets/img_user.png";
 
 const ProductReview: React.FC = () => {
 	return (
 		<div className="w-full flex flex-col justify-start items-center p-10">
-			<div className="w-[100%] bg-gray-200 p-7 rounded-3xl">
-				<div>
-					<div>
-						<img src="" alt="" />
+			<div className="w-[100%] bg-gray-200 p-7 rounded-3xl flex flex-col justify-start items-center">
+				<div className="w-full flex justify-between items-center">
+					<div className="flex justify-center items-center gap-4">
+						<img src={user} alt="user" className="w-24 h-24" />
 						<div>
 							<p>Louis Litt</p>
 							<p>Description</p>
