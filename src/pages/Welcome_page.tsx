0a78cde01d5633d55c3d29a36e5cc016bda0b01e
DevCHIDI <chidimathayas@gmail.com>
2025-04-10 12:42:40 +0100

    add navigation to login and see more buttons

--- src/pages/Welcome_page.tsx
+++ src/pages/Welcome_page.tsx
@@ -1,6 +1,8 @@
 import loginBg from "../assets/login-bg.png";
+import { useNavigate } from "react-router-dom";
 
 const Welcome_page: React.FC = () => {
+	const navigate = useNavigate();
 	return (
 		<div className="w-screen h-full">
 			<div className="w-full h-[50%]">
@@ -9,7 +11,10 @@ const Welcome_page: React.FC = () => {
 
 			<div className="w-full flex flex-col justify-center items-center">
 				<h1 className="text-8xl font-bold mb-20">PlantPot</h1>
-				<button className="bg-black font-bold text-white  text-4xl w-[300px] h-28 rounded-full py-5 px-15 mb-10">
+				<button
+					onClick={() => navigate("/home")}
+					className="bg-black font-bold text-white  text-4xl w-[300px] h-28 rounded-full py-5 px-15 mb-10"
+				>
 					Login
 				</button>
 				<button className=" text-blck font-bold text-4xl w-[300px] h-28 rounded-full py-5 px-15">
